5373f352b3cb60480e3c367d93a55cf4ae6fba03
LF-Uleth <144730637+LF-Uleth@users.noreply.github.com>
2024-12-06 11:40:13 -0700

    Update create.cc

--- create.cc
+++ create.cc
@@ -5,17 +5,20 @@
 #include <string>
 
 int main() {
+    //get n
     int n;
     std::cout << "Enter the number of points (n): ";
     std::cin >> n;
 
-std::srand(std::time(nullptr));
-std::string filename = "output" + std::to_string(n) + ".txt";
+//rand generate setup
+std::srand(std::time(nullptr)); //rand seed for generation from time
+std::string filename = "output" + std::to_string(n) + ".txt"; 
 std::ofstream outfile(filename);
 
+//create points between -10000 and 10000
 for (int i = 0; i < n; ++i) {
-        int x = std::rand() % 20001 - 10000; // Generate x in range [-10000, 10000]
-        int y = std::rand() % 20001 - 10000; // Generate y in range [-10000, 10000]
+        int x = std::rand() % 20001 - 10000;
+        int y = std::rand() % 20001 - 10000;
         outfile << "(" << x << "," << y << ")" << std::endl;
     }
 
